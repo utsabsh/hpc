d2d78b7df8306dc19b25fae94e54994ee22d9257
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2024-12-05 19:17:38 +0545

    hpc 1

---
+++ week1/ex2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "ex2.h"
+int main(){
+	//function call
+	int a=10, b=4;
+	int sum=add(a,b);
+	printf("%d",sum);
+	return 0;
+}
---
+++ week1/ws1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  for(int n = 0; n < 10; n++){
+  		if(n==8){
+  		 break;
+	  }
+  	if(n!=5){
+  		 printf("%d\n", n);
+	  }
+	  
+
+  }
+}
